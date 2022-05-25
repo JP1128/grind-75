0e33cfdeb9c3c42ec11ffe0a1cb1a5a3acc0be0f
Jae Park <jp1004.1040@gmail.com>
2022-05-25 18:41:11 -0400

    Improved the solution in 3_merge_two_sorted_lists.py to get rid of unnecessary variables

--- 3_merge_two_sorted_lists.py
+++ 3_merge_two_sorted_lists.py
@@ -10,30 +10,18 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # In case one or both are empty
-        if list1 is None: return list2
-        if list2 is None: return list1
-        
-        if list1.val < list2.val:
-            head = curr = list1
-            curr1 = list1.next
-            curr2 = list2
-        
-        else:
-            head = curr = list2
-            curr1 = list1
-            curr2 = list2.next
-        
-        while curr1 and curr2:
-            if curr1.val < curr2.val:
-                curr.next = curr1
-                curr1 = curr1.next
+        dummy = curr = ListNode()
+               
+        while list1 and list2:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
             else:
-                curr.next = curr2
-                curr2 = curr2.next
+                curr.next = list2
+                list2 = list2.next
             
             curr = curr.next
         
-        curr.next = curr1 or curr2
+        curr.next = list1 or list2
                 
-        return head
+        return dummy.next
